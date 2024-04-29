5b44f94e6ae35a20bb412ba0672f7833006089a2
Akmal Khan <akmalnawabi007@gmail.com>
2024-04-29 10:44:12 +0430

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+
+import { useState } from 'react';
+import './App.css'
+import Square from './components/Square'
+import CalculateWinner from './components/CalculateWinner';
+
+function App() {
+  const [xIsNext, setXIsNext] = useState(true)
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
+  const handleClick = (i) => {
+    if(CalculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice()
+    if(xIsNext) {
+      nextSquares[i] = "x";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares)
+    setXIsNext(!xIsNext)
+  }
+
+  const winner = CalculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <>
+      <div>{status}</div>
+      <div className="boardRow">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+      </div>
+      <div className="boardRow">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+      </div>
+      <div className="boardRow">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+      </div>
+    </>
+  );
+}
+
+export default App
